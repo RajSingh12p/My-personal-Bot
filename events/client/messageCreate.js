13304846cf3f6d7dad12ea1a7b4ac27dee40ec38
RajSingh12p <101890457+RajSingh12p@users.noreply.github.com>
2025-08-14 14:17:02 +0530

    Update messageCreate.js

--- events/client/messageCreate.js
+++ events/client/messageCreate.js
@@ -1,4 +1,5 @@
-const { Events, EmbedBuilder } = require('discord.js');
+const { Events } = require('discord.js');
+const PrefixHandler = require('../../handlers/prefixHandler');
 
 module.exports = {
   name: Events.MessageCreate,
@@ -6,26 +7,9 @@ module.exports = {
   async execute(message) {
     if (message.author.bot) return;
 
-    const mention = new RegExp(`^<@!?${message.client.user.id}>( |)$`);
-
-    if (message.content.match(mention)) {
-      try {
-        const commands = await message.client.application.commands.fetch();
-
-        const helpCommand = commands.find((cmd) => cmd.name === 'help');
-        const helpCommandId = helpCommand ? helpCommand.id : 'unknown';
-
-        const mentionEmbed = new EmbedBuilder()
-          .setColor(0x5865f2)
-          .setDescription(
-            `Hey ${message.author}, I'm ! ᴅᴏ ᴏʀ ᴅɪᴇ, I use \`/\` commands.\nCheck out my commands, type </help:${helpCommandId}>`
-          )
-          .setTimestamp();
-
-        message.reply({ embeds: [mentionEmbed] }).catch(console.error);
-      } catch (error) {
-        console.error('Error fetching commands:', error);
-      }
+    // Handle prefix commands
+    if (message.client.prefixHandler) {
+      await message.client.prefixHandler.handleMessage(message);
     }
   },
 };
